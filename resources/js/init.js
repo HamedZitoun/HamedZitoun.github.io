5ac7162da0bfe8158faee9467122554696519a9b
hzitoun <hzitoun@enseirb-matmeca.fr>
2015-06-07 18:54:04 +0200

    modifing the get username logic

--- resources/js/init.js
+++ resources/js/init.js
@@ -9,38 +9,38 @@ var userBase = new Firebase("https://resplendent-fire-5470.firebaseio.com/");
 var myUserRef = userBase.push();
 var connectedRef = new Firebase("https://resplendent-fire-5470.firebaseio.com//.info/connected");
 connectedRef.on("value", function(isOnline) {
-  if (isOnline.val()) {
-    setUserStatus("online");
-  }
-  else {
-    setUserStatus("away");
-  }
+	if (isOnline.val()) {
+		setUserStatus("online");
+	}
+	else {
+		setUserStatus("away");
+	}
 });
 $( "#join" ).click(function() {
-if(currentUser != ''){
-var currentStatus = "online";
-userBase.on("child_added", function(element) {
-    var user = element.val();
-    $("<div/>")
-      .attr("id", getMessageId(element))
-      .append('<div>' + user.username + "  is " + user.status + '<hr class="hr-clas-low" />')
-      .appendTo("#presenceDiv");
-  });
-  userBase.on("child_removed", function(element) {
-   setUserStatus("away");
-  });
-  userBase.on("child_changed", function(element) {
-    var user = element.val();
-    $("#presenceDiv").children("#" + getMessageId(element))
-      .html('<div>' + user.username + " is " + user.status + '<hr class="hr-clas-low" />')
-  });
-$('#popup').fadeOut(100);
-$("#messageInput").removeAttr('disabled');
-$("#send").removeAttr('disabled');
-}
+	if(currentUser != ''){
+		var currentStatus = "online";
+		userBase.on("child_added", function(element) {
+			var user = element.val();
+			$("<div/>")
+			.attr("id", getMessageId(element))
+			.append('<div>' + user.username + "  is " + user.status + '<hr class="hr-clas-low" />')
+			.appendTo("#presenceDiv");
+		});
+		userBase.on("child_removed", function(element) {
+			setUserStatus("away");
+		});
+		userBase.on("child_changed", function(element) {
+			var user = element.val();
+			$("#presenceDiv").children("#" + getMessageId(element))
+			.html('<div>' + user.username + " is " + user.status + '<hr class="hr-clas-low" />')
+		});
+		$('#popup').fadeOut(100);
+		$("#messageInput").removeAttr('disabled');
+		$("#send").removeAttr('disabled');
+	}
 });
 
 function setUserStatus(status) {
-  currentStatus = status;
-  myUserRef.set({ username: currentUser, status: status });
+	currentStatus = status;
+	myUserRef.set({ username: currentUser, status: status });
 }
